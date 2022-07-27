2ec5a6ee24dafb8b03a2e3e42ded39d4ae02d95a
doudou-dream <dairen_y@163.com>
2022-07-27 15:32:24 +0800

    pdf高度宽度变量抽出

--- src/uilts/pdf.js
+++ src/uilts/pdf.js
@@ -8,6 +8,8 @@ import JsPDF from 'jspdf'
  * @param title = 标题
  */
 export const htmlToPdf = (title) => {
+    const width  = 592.28// pdf宽度
+    const height = 841.89 // pdf高度
     html2Canvas(document.querySelector('#pdfDom'), {
         allowTaint: true,
     }).then(canvas => {
@@ -16,18 +18,18 @@ export const htmlToPdf = (title) => {
         //内容高度
         let contentHeight = canvas.height
         //一页pdf显示html页面生成的canvas高度,a4纸的尺寸[595.28,841.89];
-        let pageHeight    = contentWidth / 592.28 * 841.89
+        let pageHeight    = contentWidth / width * height
         //未生成pdf的html页面高度
         let leftHeight    = contentHeight
         //页面偏移
         let position      = 0
         //a4纸的尺寸[595.28,841.89]，html页面生成的canvas在pdf中图片的宽高
-        let imgWidth      = 595.28
-        let imgHeight     = 592.28 / contentWidth * contentHeight
+        let imgWidth      = width + 3
+        let imgHeight     = width / contentWidth * contentHeight
         //canvas转图片数据
         let pageData      = canvas.toDataURL('image/jpeg', 1.0)
         //新建JsPDF对象
-        let PDF           = new JsPDF('', 'pt', 'a4')
+        let PDF           = new JsPDF('p', 'pt', 'a4')
         //判断是否分页
         if (leftHeight < pageHeight) {
             PDF.addImage(pageData, 'JPEG', 0, 0, imgWidth, imgHeight)
@@ -35,7 +37,7 @@ export const htmlToPdf = (title) => {
             while (leftHeight > 0) {
                 PDF.addImage(pageData, 'JPEG', 0, position, imgWidth, imgHeight)
                 leftHeight -= pageHeight
-                position -= 841.89
+                position -= height
                 if (leftHeight > 0) {
                     PDF.addPage()
                 }
